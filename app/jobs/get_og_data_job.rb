f5baa0352794fc9cf4d2f86fee1bbfd463fae321
Dan Ellenbaum <dan.ellenbaum@stashrewards.com>
2020-06-03 18:04:14 -0700

    POC - using opengraph gem, can create a job (so it controller can call async per requirements) that grabs og image

---
+++ app/jobs/get_og_data_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Use opengraph parser gem to perform the task
+# of scraping the og data from a 3rd part url
+# given as an argument to this job
+class GetOgDataJob < ApplicationJob
+  queue_as :default
+
+  def perform(input_url: 'http://ogp.me')
+    # TODO: Determine if we want to use fallback
+    # method in case of no og data on this url
+    # for now, use "false" here to turn it off
+    og = OpenGraph.new(input_url, false)
+    # TODO: Save this to DB so the polling code
+    # can grab the image when it is populated
+    og.images
+  end
+end
